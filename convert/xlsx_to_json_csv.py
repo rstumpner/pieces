556723834309bd284902951e10d6e94aecfa44a8
Roland Stumpner <roland@stumpner.at>
2023-08-09 15:02:16 +0200

    changed cicd converter update path

--- convert/xlsx_to_json_csv.py
+++ convert/xlsx_to_json_csv.py
@@ -4,13 +4,14 @@ import pandas as pd
 # set path variables
 import_prefix = 'convert/'
 import_xlsx = 'together23_pieces_datenbank.xlsx'
-export_json = 'together23_pieces_datenbank.json'
-export_csv = 'together23_pieces_datenbank.csv'
+export_prefix = 'static/'
+export_json = 'challenges.json'
+export_csv = 'challenges.csv'
 
 print(import_xlsx)
 # Read imput 
 df = pd.read_excel (r''+import_prefix+import_xlsx,sheet_name='export')
 
 # export to dataformat
-df.to_json (r''+import_prefix+export_json, orient='records')
-df.to_csv (r''+import_prefix+export_csv,index = None, header=True)
+df.to_json (r''+export_prefix+export_json, orient='records')
+df.to_csv (r''+export_prefix+export_csv,index = None, header=True)
