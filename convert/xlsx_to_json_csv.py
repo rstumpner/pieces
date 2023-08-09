3a6c8c84aef380d8b0423b3b3efdd41c655e04b5
Roland Stumpner <roland@stumpner.at>
2023-08-09 14:33:04 +0200

    changed script xls_to_json updaet path

--- convert/xlsx_to_json_csv.py
+++ convert/xlsx_to_json_csv.py
@@ -2,14 +2,15 @@
 import pandas as pd
 
 # set path variables
+import_prefix = 'convert/'
 import_xlsx = 'together23_pieces_datenbank.xlsx'
 export_json = 'together23_pieces_datenbank.json'
 export_csv = 'together23_pieces_datenbank.csv'
 
 print(import_xlsx)
 # Read imput 
-df = pd.read_excel (r''+import_xlsx,sheet_name='export')
+df = pd.read_excel (r''+import_prefix+import_xlsx,sheet_name='export')
 
 # export to dataformat
-df.to_json (r''+export_json, orient='records')
-df.to_csv (r''+export_csv,index = None, header=True)
+df.to_json (r''+import_prefix+export_json, orient='records')
+df.to_csv (r''+import_prefix+export_csv,index = None, header=True)
