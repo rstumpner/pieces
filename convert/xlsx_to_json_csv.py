a209e7fa9231976e9e7d1e018ad8255f62b1ae0d
Roland Stumpner <roland@stumpner.at>
2023-08-09 11:30:14 +0200

    changed converter feature datasheet and path things

--- convert/xlsx_to_json_csv.py
+++ convert/xlsx_to_json_csv.py
@@ -1,4 +1,15 @@
+# import python libarys
 import pandas as pd
-df = pd.read_excel (r'together23_pieces_datenbank.xlsx')
-df.to_json (r'together23_pieces_datenbank.json', orient='records')
-df.to_csv (r'together23_pieces_datenbank.csv',index = None, header=True)
+
+# set path variables
+import_xlsx = 'together23_pieces_datenbank.xlsx'
+export_json = 'together23_pieces_datenbank.json'
+export_csv = 'together23_pieces_datenbank.csv'
+
+print(import_xlsx)
+# Read imput 
+df = pd.read_excel (r''+import_xlsx,sheet_name='export')
+
+# export to dataformat
+df.to_json (r''+export_json, orient='records')
+df.to_csv (r''+export_csv,index = None, header=True)
